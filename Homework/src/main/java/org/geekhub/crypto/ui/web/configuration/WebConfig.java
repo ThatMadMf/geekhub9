7793360e19e68d12cb0beca6295dea8f19eb891b
Maksym Onoshko <maxdarksiders@gmail.com>
2020-02-15 15:35:01 +0200

    L19 Swagger 2 ui setup

--- Homework/src/main/java/org/geekhub/crypto/ui/web/configuration/WebConfig.java
+++ Homework/src/main/java/org/geekhub/crypto/ui/web/configuration/WebConfig.java
@@ -6,10 +6,7 @@ import org.geekhub.crypto.ui.web.iterceptor.RequestLogger;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @EnableWebMvc
 @Configuration
@@ -32,5 +29,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
