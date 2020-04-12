08936bc319279778df3a728f0c75da000810ebb9
Maksym Onoshko <onoshkomax@gmail.com>
2020-04-12 15:19:23 +0300

    CW updated error page

--- Reddit/src/main/java/org/geekhub/reddit/exception/WebExceptionHandler.java
+++ Reddit/src/main/java/org/geekhub/reddit/exception/WebExceptionHandler.java
@@ -2,12 +2,8 @@ package org.geekhub.reddit.exception;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class WebExceptionHandler implements ErrorController {
@@ -18,23 +14,8 @@ public class WebExceptionHandler implements ErrorController {
 
     @GetMapping("/error")
     @ExceptionHandler
-    public String exceptionHandler(HttpServletRequest request, Model model) {
-        int status = (int) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        String message;
-        if (status == 500) {
-            message = "Internal Server Error. We'll fix that";
-        } else if (status == 403) {
-            message = "Forbidden.";
-        } else if (status == 404) {
-            message = "Not Found";
-        } else {
-            message = "Unknown Error. Its not our fault.";
-        }
-
-        model.addAttribute("status", status);
-        model.addAttribute("message", message);
-
-        return "thymeleaf/ErrorPage";
+    public String exceptionHandler() {
+        return "ErrorPage";
     }
 }
 
